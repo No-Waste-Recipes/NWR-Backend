d00d6ea0ddef5db349883aaafeb8cdeb5167d096
joep <joepboekhold@gmail.com>
2021-02-19 15:58:56 +0100

    Added excluded for ingredients

--- src/recipes/models.js
+++ src/recipes/models.js
@@ -13,7 +13,7 @@ exports.RecipeModel = void 0;
 const mysql_1 = require("../config/mysql");
 class RecipeModel {
     constructor() {
-        this.tableName = 'recipes';
+        this.tableName = 'recipe';
         this.getAllRecipes = (params = {}) => __awaiter(this, void 0, void 0, function* () {
             let sql = `SELECT * FROM ${this.tableName}`;
             return yield new mysql_1.DBconnection().query(sql, '');
